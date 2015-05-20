ae461fd611b226d02721ef37f64e0b31afe867f5
Joe Schafer <joe@jschaf.com>
2015-05-19 20:01:07 -0400

    fix(esup-child): REVERT remove string name to esup-result constructor
    This reverts commit 2186a5f07c45a5a16bb2ff9cc01e1593961ef60f.

--- esup-child.el
+++ esup-child.el
@@ -233,7 +233,8 @@ Returns a list of class `esup-result'."
       ;; of results.
       (setq benchmark (benchmark-run (eval sexp)))
       (setq esup--profile-results
-            (list (esup-result :file file-name
+            (list (esup-result "esup-result"
+                               :file file-name
                                :expression-string sexp-string
                                :start-point start :end-point end
                                :line-number line-number
